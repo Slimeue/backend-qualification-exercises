8eee748baad27d46a3589077211935bae440bba0
Kevinroy <sc.kevinroy@gmail.com>
2024-05-08 20:57:34 +0800

    update: l1 p2

--- src/level1/problem2/index.ts
+++ src/level1/problem2/index.ts
@@ -1,10 +1,10 @@
-import { th } from "@faker-js/faker";
 import { randomBytes } from "crypto";
 
 export class ObjectId {
   private data: Buffer;
   private static prevTimeStamp: number;
   private static counter: number = 0;
+  private static random = randomBytes(4);
 
   constructor(type: number, timestamp: number) {
     /**
@@ -16,13 +16,15 @@ export class ObjectId {
     this.data.writeUInt8(type, 0);
 
     if (ObjectId.prevTimeStamp === timestamp) {
-      ObjectId.counter = (ObjectId.counter + 1) % 0xffffff;
+      ObjectId.counter++;
     } else {
       ObjectId.counter = Math.floor(Math.random() * 0xffffff);
       ObjectId.prevTimeStamp = timestamp;
     }
 
-    this.data.writeBigUInt64BE(BigInt(timestamp), 0);
+    this.data.writeUIntBE(timestamp, 1, 6);
+
+    ObjectId.random.copy(this.data, 8);
     this.data.writeUIntBE(ObjectId.counter, 11, 3);
   }
 
