c77ac4589236d613d8e35dda1abce5a1d6b400dc
Kevinroy <sc.kevinroy@gmail.com>
2024-05-08 16:58:20 +0800

    l1 p2 l2 p1

--- src/level1/problem2/index.ts
+++ src/level1/problem2/index.ts
@@ -1,17 +1,37 @@
+import { th } from "@faker-js/faker";
+import { randomBytes } from "crypto";
+
 export class ObjectId {
   private data: Buffer;
+  private static prevTimeStamp: number;
+  private static counter: number = 0;
 
   constructor(type: number, timestamp: number) {
     /**
      * insert your code here
      */
+
+    this.data = Buffer.alloc(14);
+
+    this.data.writeUInt8(type, 0);
+
+    if (ObjectId.prevTimeStamp === timestamp) {
+      ObjectId.counter = (ObjectId.counter + 1) % 0xffffff;
+    } else {
+      ObjectId.counter = Math.floor(Math.random() * 0xffffff);
+      ObjectId.prevTimeStamp = timestamp;
+    }
+
+    this.data.writeBigUInt64BE(BigInt(timestamp), 0);
+    randomBytes(4).copy(this.data, 1);
+    this.data.writeUIntBE(ObjectId.counter, 11, 3);
   }
 
   static generate(type?: number): ObjectId {
     return new ObjectId(type ?? 0, Date.now());
   }
-  
-  toString(encoding?: 'hex' | 'base64'): string {
-    return this.data.toString(encoding ?? 'hex');
+
+  toString(encoding?: "hex" | "base64"): string {
+    return this.data.toString(encoding ?? "hex");
   }
 }
--- src/level2/problem1/index.ts
+++ src/level2/problem1/index.ts
@@ -1,11 +1,20 @@
 export class ExecutionCache<TInputs extends Array<unknown>, TOutput> {
-  constructor(private readonly handler: (...args: TInputs) => Promise<TOutput>) {}
-  
+  constructor(
+    private readonly handler: (...args: TInputs) => Promise<TOutput>
+  ) {}
+  private cache: Map<string, Promise<TOutput>> = new Map();
+
   async fire(key: string, ...args: TInputs): Promise<TOutput> {
     /**
      * insert your code here
      */
-  
-    return;
+
+    if (this.cache.has(key)) {
+      return this.cache.get(key) as Promise<TOutput>;
+    }
+
+    const promise = this.handler(...args);
+    this.cache.set(key, promise);
+    return promise;
   }
 }
